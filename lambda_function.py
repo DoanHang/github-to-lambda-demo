945b71f48a5ddc1c693224b432075f605c9f688c
devops-lab-at-541253215789 <doanhang.mis@gmail.com>
2022-07-11 15:26:18 +0700

    added demo lambda code

---
+++ lambda_function.py
@@ -0,0 +1,7 @@
+import pandas as pd
+def lambda_handler(event, context):
+    d= {'name' :['Gigi Hadid', 'Alice'], 'age': [30, 20]}
+    df= pd.DataFrame(data=d)
+    print(df)
+    print('Done!!!')
+    
